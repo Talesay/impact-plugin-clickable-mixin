eea2e2b266f176660076e1f4c1e689512c71c410
FelipeBudinich <fbudinichd@gmail.com>
2015-02-18 15:39:18 -0300

    removed conflicted version

--- mixin.js
+++ mixin.js
@@ -8,28 +8,20 @@ ig.module(
 		clickedInside: false,
 		clickName: 'click',
 		updateClickable: function () {
-			var clicked = ig.input.state(this.clickName);
-			if (!this.oldClicked && clicked && this.isMouseOver()) {
-				this.clickedInside = true;
+			var over = this.overClickable(),
+				out = !over,
+				pressed = (ig.input.state(this.clickName) === true),
+				released = (ig.input.state(this.clickName) === false);
+			
+			if (pressed && !this.oldClick && over) {
+				this.oldClick = true;
+				this.pressed();
+			} else if (released && this.oldClick && over) {
+				this.oldClick = false;
+				this.released();
 			}
-			if (this.clickedInside && this.overClickable()) {
-				if (clicked && !this.oldClick) {
-					this.pressed();
-				} else if (clicked) {
-					this.down();
-				} else if (this.oldClick) {
-					this.released();
-				}
-			}
-			if (this.oldClick && !clicked) {
-				this.clickedInside = false;
-			}
-			this.oldClicked = clicked;
 		},
-		down: function () {},
-		pressed: function () {},
-		released: function () {},
-		isMouseOver: function () {
+		overClickable: function () {
 			return ig.input.mouse.x + ig.game.screen.x > this.pos.x && ig.input.mouse.x + ig.game.screen.x < this.pos.x + this.size.x && ig.input.mouse.y + ig.game.screen.y > this.pos.y && ig.input.mouse.y + ig.game.screen.y < this.pos.y + this.size.y;
 		}
 	};
